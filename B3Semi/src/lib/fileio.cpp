f237c5e18dbeffce34011131710619585c5c9f6b
Ilekaede <sakurai@LAPTOP-J58SQ5JI>
2023-10-10 12:49:46 +0900

    fix: SEMI_wiki.mdの詳細化

--- B3Semi/src/lib/fileio.cpp
+++ B3Semi/src/lib/fileio.cpp
@@ -29,26 +29,26 @@ ImageMat read_image(const std::string &filename)
 
   std::cout << "file_size: " << size << std::endl;
   image_file.seekg(0, std::ios::beg); // 読み込みファイルの先頭に戻る
-  image_file.read(reinterpret_cast<char *>(image_mat.m_header.data()), ■); // ファイルの上から■番目までを読み込む
+  image_file.read(reinterpret_cast<char *>(image_mat.m_header.data()), '*'); // ファイルの上から'*'番目までを読み込む
 
-  image_mat.m_width = image_mat.m_header[■] +
-                      image_mat.m_header[■] * 256 +
-                      image_mat.m_header[■] * 256 * 256 +
-                      image_mat.m_header[■] * 256 * 256 * 256;
+  image_mat.m_width = image_mat.m_header['*'] +
+                      image_mat.m_header['*'] * 256 +
+                      image_mat.m_header['*'] * 256 * 256 +
+                      image_mat.m_header['*'] * 256 * 256 * 256;
 
-  image_mat.m_height = image_mat.m_header[■] +
-                       image_mat.m_header[■] * 256 +
-                       image_mat.m_header[■] * 256 * 256 +
-                       image_mat.m_header[■] * 256 * 256 * 256;
+  image_mat.m_height = image_mat.m_header['*'] +
+                       image_mat.m_header['*'] * 256 +
+                       image_mat.m_header['*'] * 256 * 256 +
+                       image_mat.m_header['*'] * 256 * 256 * 256;
 
-  image_mat.m_length = image_mat.m_header[■] +
-                       image_mat.m_header[■] * 256 +
-                       image_mat.m_header[■] * 256 * 256 +
-                       image_mat.m_header[■] * 256 * 256 * 256;
+  image_mat.m_length = image_mat.m_header['*'] +
+                       image_mat.m_header['*'] * 256 +
+                       image_mat.m_header['*'] * 256 * 256 +
+                       image_mat.m_header['*'] * 256 * 256 * 256;
 
   auto &pixel_data = image_mat.m_pixels;
   pixel_data.resize(image_mat.m_length);
-  image_file.read(reinterpret_cast<char *>(pixel_data.data()), ■);
+  image_file.read(reinterpret_cast<char *>(pixel_data.data()), '*');
 
   std::cout << "loaded image :" << image_mat.m_width << " x " << image_mat.m_height << std::endl;
 
@@ -81,28 +81,28 @@ void write_image(const ImageMat &image, const std::string &filename)
   // 処理によって画像サイズが変化した場合に必要
   auto image_header = image.m_header;
   int width = image.m_width;
-  for (int i = ■; i <= ■; i++)
+  for (int i = '*'; i <= '*'; i++)
   {
     // 処理を記述
   }
 
   int height = image.m_height;
-  for (int i = ■; i <= ■; i++)
+  for (int i = '*'; i <= '*'; i++)
   {
     //処理を記述
   }
 
-  image_header[■] = image.m_ch;
+  image_header['*'] = image.m_ch;
 
   int length = image.m_length;
-  for (int i = ■; i <= ■; i++)
+  for (int i = '*'; i <= '*'; i++)
   {
     //処理を記述
   }
 
   auto image_pixels = image.m_pixels;
-  image_file.write(reinterpret_cast<char *>(image_header.data()), ■);
-  image_file.write(reinterpret_cast<char *>(image_pixels.data()), ■);
+  image_file.write(reinterpret_cast<char *>(image_header.data()), '*');
+  image_file.write(reinterpret_cast<char *>(image_pixels.data()), '*');
 
   image_file.close();
 
